93403573f5c95ec020487c6ca7a838ed1170b7dd
EC2 Default User <ec2-user@ip-172-15-3-148.ad.imagin3d.local>
2022-02-10 11:43:13 +0000

    js vulns

--- vulnerabilities.js
+++ vulnerabilities.js
@@ -3,3 +3,19 @@ let credential = "123235425sdfsdfsdyvfsdf"
 let password = "passP9)^11912413"
 
 let pass = "password"
+
+
+// sql injection
+const express = require('express');
+const db = require('./db');
+
+const router = express.Router();
+
+router.get('/email', (req, res) => {
+  db.query('SELECT email FROM users WHERE id = ' + req.query.id);
+    .then((record) => {
+      // logical flow
+      res.send(record[0]);
+    })
+});
+
