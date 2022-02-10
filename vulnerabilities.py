6553a5a1046c0a3a5d2315a74316f6837a8864ae
EC2 Default User <ec2-user@ip-172-15-3-148.ad.imagin3d.local>
2022-02-10 10:59:45 +0000

    python vulns

---
+++ vulnerabilities.py
@@ -0,0 +1,25 @@
+# exposed credentials
+credential = "0145VXki111"
+password = 10
+
+# injection - Exploiting Eval() and Exec()
+compute = input('\nYour expression? => ')
+if not compute:
+    print ("No input")
+else:
+    print ("Result =", eval(compute))
+
+# injection - exploiting input
+admin_pass = get_user_pass("admin")
+if password == input("Please enter your password"):
+  login()
+else:
+  print ("Password is incorrect!")
+  
+# injection - OS commands 
+address = request.args.get("address")
+cmd = "ping -c 1 %s" % address
+subprocess.Popen(cmd, shell=True)
+
+# using assert()
+assert sum([1, 2, 3]) == 6, "Should be 6"
